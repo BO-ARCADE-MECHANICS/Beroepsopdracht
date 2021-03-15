b597fb21b0c21b9955da7a1e5bbb1a6c02daf9d0
michelle frankfort <j.s.frankfort@gmail.com>
2021-03-15 23:31:26 +0100

    added movement, character, animations and more

---
+++ Assets/Scripts/player_script/playermovment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovment : MonoBehaviour
+{
+
+    public float MoveSpeed = 5f;
+
+    public Rigidbody2D PlayerBody;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        PlayerBody.MovePosition(PlayerBody.position + movement * MoveSpeed * Time.fixedDeltaTime);
+    }
+}

3945f79166fc6d4068a049f9fa3ae68cf42ef09c
Monique Wienholds <snuffelien@gmail.com>
2021-04-03 22:42:26 +0200

    game over

---
+++ Assets/Scripts/player_script/restart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restart : MonoBehaviour
+{
+    public GameObject gameOverText, RestartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverText.SetActive(false);
+        RestartButton.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Enemy"))
+        {
+            gameOverText.SetActive(true);
+            RestartButton.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}

14a432577f4b610c2a71027af95750804cbdec68
Monique Wienholds <snuffelien@gmail.com>
2021-04-04 19:47:52 +0200

    Health system toegevoegd

--- Assets/Scripts/player_script/restart.cs
+++ Assets/Scripts/player_script/restart.cs
@@ -6,9 +6,28 @@ public class restart : MonoBehaviour
 {
     public GameObject gameOverText, RestartButton;
 
+    public GameObject heart1, heart2, heart3;
+    public int playerHealth = 3;
+    int playerLayer, enemyLayer;
+    bool coroutineAllowed = true;
+    Color color;
+    Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerLayer = this.gameObject.layer;
+        enemyLayer = LayerMask.NameToLayer("Enemy");
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false);
+        heart1 = GameObject.Find("Heart01");
+        heart2 = GameObject.Find("Heart02");
+        heart3 = GameObject.Find("Heart03");
+        heart1.gameObject.SetActive(true);
+        heart2.gameObject.SetActive(true);
+        heart3.gameObject.SetActive(true);
+        rend = GetComponent<Renderer>();
+        color = rend.material.color;
+
         gameOverText.SetActive(false);
         RestartButton.SetActive(false);
     }
@@ -17,9 +36,45 @@ public class restart : MonoBehaviour
     {
         if (col.gameObject.tag.Equals("Enemy"))
         {
-            gameOverText.SetActive(true);
-            RestartButton.SetActive(true);
-            gameObject.SetActive(false);
+            playerHealth -= 1;
+            switch (playerHealth)
+            {
+                case 2:
+                    heart3.gameObject.SetActive(false);
+                    if (coroutineAllowed)
+                        StartCoroutine("Immortal");
+                    break;
+                case 1:
+                    heart2.gameObject.SetActive(false);
+                    if (coroutineAllowed)
+                        StartCoroutine("Immortal");
+                    break;
+                case 0:
+                    heart1.gameObject.SetActive(false);
+                    if (coroutineAllowed)
+                        StartCoroutine("Immortal");
+                    break;
+            }
+
+            if (playerHealth < 1)
+            {
+                gameOverText.SetActive(true);
+                RestartButton.SetActive(true);
+                gameObject.SetActive(false);
+            }
         }
+    }
+
+    IEnumerator Immortal()
+    {
+        coroutineAllowed = false;
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, true);
+        color.a = 0.5f;
+        rend.material.color = color;
+        yield return new WaitForSeconds(1);
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false);
+        color.a = 1f;
+        rend.material.color = color;
+        coroutineAllowed = true;
     }
 }
